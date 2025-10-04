77b7ef257e5aa3f1aca49905dfaab7363fb8be1b
mxkcpy <mpawlos20@gmail.com>
2025-10-04 16:46:44 +0200

    initial commit

---
+++ src/main/java/team/four/nbd/NbdApplication.java
@@ -0,0 +1,13 @@
+package team.four.nbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/four/nbd/NbdApplicationTests.java
@@ -0,0 +1,13 @@
+package team.four.nbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
