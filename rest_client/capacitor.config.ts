c762747f2c85426bae0b3e340be0691175fb6255
micdom4 <domagalamichal04@gmail.com>
2026-01-12 15:39:21 +0100

    Fixed mobile with backend communication

--- rest_client/capacitor.config.ts
+++ rest_client/capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'team.four.pas',
   appName: 'polVirtApp',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'http',
+    cleartext: true
+  }
 };
 
 export default config;
