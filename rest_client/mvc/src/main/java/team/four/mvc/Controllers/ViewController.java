adcf2b6ce67a0aa1b5c28da414a39483031f7dbb
mxkcpy <mpawlos20@gmail.com>
2026-01-24 20:15:00 +0100

    Initial Commit

--- rest_client/mvc/src/main/java/team/four/mvc/Controllers/ViewController.java
+++ rest_client/mvc/src/main/java/team/four/mvc/Controllers/ViewController.java
@@ -1,13 +1,16 @@
 package team.four.mvc.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import team.four.pas.controllers.DTOs.UserType;
 
 @Controller
 public class ViewController {
 
     @GetMapping("/superModernSite")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("userTypeValues", UserType.values());
         return "index";
     }
 
--- rest_client/mvc/src/test/java/team/four/mvc/MvcApplicationTests.java
+++ rest_client/mvc/src/test/java/team/four/mvc/MvcApplicationTests.java
@@ -24,4 +24,5 @@ class MvcApplicationTests {
                 .andExpect(view().name("index"));
     }
 
+
 }
